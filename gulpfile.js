ca88c440dcff70b81d32f0e27d4e1e3bd615ba54
Andrew Minion <andrew@andrewrminion.com>
2015-11-23 16:59:38 -0500

    update project info and add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var $    = require('gulp-load-plugins')();
 
 var sassPaths = [
@@ -15,9 +16,16 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['./scss/**/*.scss'], ['sass']);
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "dev.abc.dev"
+    });
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("**/**.html").on('change', browserSync.reload);
 });
+
+gulp.task('default', ['browser-sync']);
